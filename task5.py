bb73795f36f9dd9101de5d7bf201c7bb12f167bb
Emily Qin <eyq3@duke.edu>
2020-09-06 23:00:19 -0500

    hex to bytes

--- task5.py
+++ task5.py
@@ -1,3 +1,13 @@
-from Crypto.Cipher import AES
+from Crypto.Util import *
+from Crypto.Cipher import *
+from padding import pad, unpad
+from base64 import b64encode
 
-iv = "aabbccddeeff00998877665544332211"
+hex_iv = 'aabbccddeeff00998877665544332211'
+iv_bytes = bytes.fromhex(hex_iv)
+
+plaintext = b"you captured the flag"
+padded = pad(plaintext,16)
+
+key = b'Sixteen byte key'
+cipher = AES.new(key, AES.MODE_CBC, iv_bytes)
